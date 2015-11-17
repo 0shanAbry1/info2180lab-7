ccfe8a68a3da98471c5c77a1877866f4056f7bc1
Shanice A. Bryan <indi.shanbry@gmail.com>
2015-11-17 20:34:47 +0000

    1st Commit

--- request.php
+++ request.php
@@ -4,6 +4,7 @@
 # accept a term (keyword)
 # respond with a value
 $query = $_REQUEST['q'];
+$all = $_REQUEST['all'];
 
 $definition = [
     "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
@@ -11,8 +12,20 @@ $definition = [
     "ajax" => "technique which involves the use of javascript and xml"
 ];
 
-print $query;
-print "<br>";
-print($definition[$query]);
+if($all==true and $query==null){
+    header("Content-type: text/xml");
+    $str = '<entries>';
+    foreach($definition as $key=>$value){
+        $str.='<definition name=\''.$key.'\'>'.$value.'</definition>';
+    }
+    $str.='</entries>';
+    $xml = new SimpleXMLElement($str);
+    echo $xml->asXML();
+}
+else{
+ print $query;
+ print"<br>";
+ print($definition[$query]);
+}
 
 ?>
