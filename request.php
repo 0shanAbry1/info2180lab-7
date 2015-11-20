212e555c5a2aec08b9a6005e578bae4bcea094fb
Shanice A. Bryan <indi.shanbry@gmail.com>
2015-11-20 04:33:21 +0000

    Final commit

--- request.php
+++ request.php
@@ -6,26 +6,42 @@
 $query = $_REQUEST['q'];
 $all = $_REQUEST['all'];
 
-$definition = [
-    "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "a place that sells alcholic beverages",
-    "ajax" => "technique which involves the use of javascript and xml"
+$definitions = [
+    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+    "bar" => "A place that sells alcholic beverages",
+    "ajax" => "Technique which involves the use of javascript and xml"
+];
+
+$examples = [
+    "definition" => "I know not what the definition of a Sucubbus is.",
+    "bar" => "Everyday after work, Bob goes to the bar to have a drink with her friends.",
+    "ajax" => "Ajax stands for Asynchronous Javascript and XML."
+];
+
+$authors = [
+    "definition" => "- Ally AppleBottom",
+    "bar" => "- Sugarpie Cinnamon",
+    "ajax" => "- Candycrush Saga"
 ];
 
 if($all==true and $query==null){
     header("Content-type: text/xml");
-    $str = '<entries>';
-    foreach($definition as $key=>$value){
-        $str.='<definition name=\''.$key.'\'>'.$value.'</definition>';
+    
+    $str = '<entries>'; //xml - open tag
+    foreach($definitions as $key=>$value){
+        $str.='<definitions name = \''.$key.'\'>'.$value.'</definitions>';
+        $str.='<examples>'.'Example: '.$examples[$key].'</examples>';
+        $str.='<authors>'.$authors[$key].'</authors>';
     }
-    $str.='</entries>';
+    $str.='</entries>'; //xml - close tag
+    
     $xml = new SimpleXMLElement($str);
     echo $xml->asXML();
 }
 else{
- print $query;
- print"<br>";
- print($definition[$query]);
+    print"<br>"; print ($query.' - '.$definitions[$query]); print"<br>";print"<br>";
+    print('Example: '.$examples[$query]); print"<br>";print"<br>";
+    print($authors[$query]);
 }
 
 ?>
